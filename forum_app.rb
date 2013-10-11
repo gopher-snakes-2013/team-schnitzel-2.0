708af7859aa5920b9b8ec73775fa1d03a4d81070
Charles Amoako <Charles@Charless-MacBook-Pro.local>
2013-10-11 16:05:01 -0700

    release 2: add comments to post & display on posts page

--- forum_app.rb
+++ forum_app.rb
@@ -12,15 +12,22 @@ end
 set :database, ENV['DATABASE_URL']
 
 get '/' do
-    @posts = Post.all
+    @posts = Post.all.reverse
     erb :index
 end
 
 post '/' do
-    Post.create(params.symbolize_keys)
+    Post.create(title: params[:title], text_body: params[:text_body])
     redirect '/'
 end
 
 get '/post/:post_id' do
+	@post = Post.find(params[:post_id])
+	@comments = Comment.where("post_id = ?", params[:post_id])
 	erb :post
+end
+
+post '/post/:post_id' do
+	Comment.create(text_body: params[:text_body], post_id: params[:post_id])
+    redirect "/post/1"
 end
